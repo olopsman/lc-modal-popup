7785c1398646e0f11734c0ac3ee5791f2588ef53
Paulo Orquillo <paulo.orquillo@davanti.co.nz>
2018-03-06 22:14:01 +1300

    initial commit

---
+++ Dang_Modal_PopupController.js
@@ -0,0 +1,23 @@
+({
+	cancelBtn : function(component, event, helper) {
+		component.set("v.displayModal", true);
+	},
+	saveBtn : function(component, event, helper) {
+		//create server calls
+	},
+	closeBtn : function(component, event, helper) {
+        component.set("v.displayModal", false);
+	},
+	yesBtn : function(componet, event, helper) {
+       	//var urlEvent = $A.get("e.force:navigate....");
+       	//urlEvent.setParams({
+    	//set params
+    	//});
+    	//urlEvent.fire();
+        component.set("v.displayModal", false);
+	},
+	noBtn : function(component, event, helper) {
+        component.set("v.displayModal", false);
+	}
+
+})
